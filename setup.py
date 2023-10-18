18e9f53922889e7b5abbfce1fe50d8e986d9cde8
Amit <amitchauhanpce@gmail.com>
2023-10-18 20:56:32 +0530

    The Sceond Commit

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+HYPEN_E_DOT = '-e .'
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() and req.strip() != HYPEN_E_DOT]
+
     return requirements
 
-setup(
-name='mlproject',
-version='0.0.1',
-author='Amit',
-author_email='amitchauhanpce6@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
 
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Amit',
+    author_email='amitchauhanpce6@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
